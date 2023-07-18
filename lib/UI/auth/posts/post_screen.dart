5e0fa052cd8ba07edd7b75bff4e5299e4a3f5140
vinay <vinayvish2567@gmail.com>
2023-07-19 03:35:48 +0530

    Fetch data by using FireBaseAnimatedList or streamBuilder, Build  search filter

--- lib/UI/auth/posts/post_screen.dart
+++ lib/UI/auth/posts/post_screen.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_connections_tutorials/UI/auth/login_screen.dart';
 import 'package:firebase_connections_tutorials/UI/auth/posts/add_posts.dart';
 import 'package:firebase_connections_tutorials/Utils/Utils.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 
 class PostScreen extends StatefulWidget {
@@ -13,11 +15,16 @@ class PostScreen extends StatefulWidget {
 
 class _PostScreenState extends State<PostScreen> {
   final auth = FirebaseAuth.instance;
+  final ref = FirebaseDatabase.instance.ref('Post');
+  final searchFilter = TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          automaticallyImplyLeading: false,
+          centerTitle: true,
           actions: [
             IconButton(
               onPressed: () {
@@ -35,12 +42,84 @@ class _PostScreenState extends State<PostScreen> {
           ],
           title: const Center(child: Text('Post')),
         ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: TextFormField(
+                controller: searchFilter,
+                decoration: InputDecoration(
+                  hintText: 'Search',
+                  border: OutlineInputBorder(),
+                ),
+                onChanged: (String value) {
+                  setState(() {});
+                },
+              ),
+            ),
+            Expanded(
+              child: FirebaseAnimatedList(
+                  query: ref,
+                  defaultChild: Text('Loading'),
+                  itemBuilder: (context, snapshot, animation, index) {
+                    final title = snapshot.child('title').value.toString();
+
+                    if (searchFilter.text.isEmpty) {
+                      return ListTile(
+                        title: Text(snapshot.child('title').value.toString()),
+                        subtitle: Text(
+                          snapshot.child('id').value.toString(),
+                        ),
+                      );
+                    } else if (title
+                        .toLowerCase()
+                        .contains(searchFilter.text.toLowerCase().toString())) {
+                      return ListTile(
+                        title: Text(snapshot.child('title').value.toString()),
+                        subtitle: Text(
+                          snapshot.child('id').value.toString(),
+                        ),
+                      );
+                    } else {
+                      return Container();
+                    }
+                  }),
+            ),
+          ],
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => AddPostScreen(),
-            ));
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => AddPostScreen(),
+                ));
           },
           child: Icon(Icons.add),
         ));
   }
 }
+
+// Expanded(
+// child: StreamBuilder(
+// stream: ref.onValue,
+// builder: (context, AsyncSnapshot<DatabaseEvent> snapshot) {
+// if (!snapshot.hasData) {
+// return CircularProgressIndicator();
+// } else {
+// Map<dynamic, dynamic> map =
+// snapshot.data!.snapshot.value as dynamic;
+// List<dynamic> list = [];
+// list.clear();
+// list = map.values.toList();
+//
+// return ListView.builder(
+// itemCount: snapshot.data!.snapshot.children.length,
+// itemBuilder: (context, index) {
+// return ListTile(
+// title: Text(list[index]['title']),
+// subtitle: Text(list[index]['id']),
+// );
+// });
+// }
+// })),
